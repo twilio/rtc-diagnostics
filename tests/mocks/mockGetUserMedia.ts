518972831534d4255ba727e9b2234b6cc40f749c
Michael Huynh <mhuynh@twilio.com>
2020-02-18 13:27:21 -0800

    Fix lint.

--- tests/mocks/mockGetUserMedia.ts
+++ tests/mocks/mockGetUserMedia.ts
@@ -3,7 +3,7 @@ import { MockMediaStream } from './MockMediaStream';
 export function mockGetUserMediaFactory(options: MockGetUserMediaOptions = {}) {
   return async function mockGetUserMedia() {
     return options.mediaStream || new MockMediaStream();
-  }
+  };
 }
 
 export interface MockGetUserMediaOptions {
