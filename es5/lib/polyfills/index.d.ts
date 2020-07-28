bf440c73b4ec4db58a1652b3d4efdafb98fc73b4
twilio-ci <twilio-ci@twilio.com>
2020-07-28 16:49:21 +0000

    1.0.0-dev

--- es5/lib/polyfills/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { AudioPolyfill as Audio, AudioUnsupportedError, } from './Audio';
-export { AudioContextPolyfill as AudioContext, AudioContextUnsupportedError, } from './AudioContext';
-export { enumerateDevicesPolyfill as enumerateDevices, enumerateDevicesUnsupportedMessage, EnumerateDevicesUnsupportedError, getDefaultDevices, } from './enumerateDevices';
-export { getUserMediaPolyfill as getUserMedia, GetUserMediaUnsupportedError, } from './getUserMedia';
