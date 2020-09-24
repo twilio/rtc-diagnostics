244ff5f69ee2fbb27529b96cd6e9bc3bc02106fa
charliesantos <csantos@twilio.com>
2020-09-24 11:08:32 -0700

    Fixing build

--- scripts/env.js
+++ scripts/env.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
-const creds = fs.existsSync('../credentials.json')
-  ? JSON.parse(fs.readFileSync('../credentials.json'))
+const path = require('path');
+
+const credsPath = path.join(__dirname, '../credentials.json');
+const creds = fs.existsSync(credsPath)
+  ? JSON.parse(fs.readFileSync(credsPath))
   : { };
 
 if (!process.env.ACCOUNTSID) {
-  process.env.ACCOUNTSID = creds.ACCOUNTSID;
+  process.env.ACCOUNTSID = creds.accountSid;
 }
 
 if (!process.env.AUTHTOKEN) {
-  process.env.AUTHTOKEN = creds.AUTHTOKEN;
+  process.env.AUTHTOKEN = creds.authToken;
 }
