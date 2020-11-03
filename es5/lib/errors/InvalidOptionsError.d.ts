8af46b913664cc2822947315ba017ec3255529e3
twilio-ci <twilio-ci@twilio.com>
2020-11-03 21:43:57 +0000

    1.0.1-dev

--- es5/lib/errors/InvalidOptionsError.d.ts
+++
@@ -1,9 +0,0 @@
-import { DiagnosticError } from './DiagnosticError';
-/**
- * @internalapi
- * Error that is thrown when there are invalid options passed to a test.
- */
-export declare class InvalidOptionsError<T extends Record<string, any>> extends DiagnosticError {
-    reasons: Partial<Record<keyof T, string[]>>;
-    constructor(reasons: Partial<Record<keyof T, string[]>>);
-}
